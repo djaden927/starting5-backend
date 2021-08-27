a42de98821b242dc2a66e4dc7d696b81da294c60
djaden927 <djaden92@gmail.com>
2021-08-27 13:04:54 -0600

    migrations and database setup

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -4,10 +4,22 @@ exports.up = async (knex) => {
       users.increments('user_id')
       users.string('username', 200).notNullable()
       users.string('password', 200).notNullable()
-      users.timestamps(false, true)
+    })
+  await knex.schema
+    .createTable('teams', (teams) => {
+      teams.string('PG', 40).notNullable()
+      teams.string('SG', 40).notNullable()
+      teams.string('SF', 40).notNullable()
+      teams.string('PF', 40).notNullable()
+      teams.string('C', 40).notNullable()
+      teams.integer('wins')
+      teams.integer('losses')
+      teams.integer('owner_name').notNullable()
+      teams.foreign('owner_name').references('user_id').inTable('users')
     })
 }
 
 exports.down = async (knex) => {
   await knex.schema.dropTableIfExists('users')
+  await knex.schema.dropTableIfExists('teams')
 }
